44ae77a39b39ba7529a6cb156b1af8b624627a60
smriddell12 <56254882+smriddell12@users.noreply.github.com>
2020-02-16 14:40:30 -0500

    commit

---
+++ src/main/java/com/mycompany/project1/Inventory.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.project1;
+import java.util.ArrayList;
+/**
+ *
+ * @author Sarah
+ */
+public class Inventory {
+    ArrayList<String> db = new ArrayList<>();
+    
+    
+}
