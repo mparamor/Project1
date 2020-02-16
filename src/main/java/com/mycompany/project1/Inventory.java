e729e0ef011eb7781f0db365eaf9c144eb4b25bc
smriddell12 <56254882+smriddell12@users.noreply.github.com>
2020-02-16 15:26:57 -0500

    commit

--- src/main/java/com/mycompany/project1/Inventory.java
+++ src/main/java/com/mycompany/project1/Inventory.java
@@ -11,6 +11,21 @@ import java.util.ArrayList;
  */
 public class Inventory {
     ArrayList<String> db = new ArrayList<>();
+    int b = db.size();
     
-    
-}
+    public void diaplay(String p){
+        for(int i=0;i<b;i++){
+            String s = db.get(i);
+            if (s.contains(p)){
+                String[] det = s.split(",");
+                System.out.print(det[0]+", ");
+                String m = det[4];
+                if (m.equals("true")){
+                    System.out.print(det[3]);}
+                else
+                    System.out.print(det[2]);
+                }
+            }
+        }
+    }
+
